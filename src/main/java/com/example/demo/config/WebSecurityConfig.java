1c80222bb4cd9ae1c59f8a0c8233e065109b2cc6
Adrian <alextravc.342@gmail.com>
2024-06-16 09:49:17 -0500

    WebSecurityConfig + JwtAppConfig + JwtAuthFilter + JwtAuthEntryPoint + SecurityErrorResponse

---
+++ src/main/java/com/example/demo/config/WebSecurityConfig.java
@@ -0,0 +1,53 @@
+package com.example.demo.config;
+
+// toda app con Spring Security al Iniciar Siempre buscara el   SecurityFilterChain   q configura toda la seguridad http de la app
+
+
+import com.example.demo.auth.jwt.JwtAuthEntryPoint;
+import com.example.demo.auth.jwt.JwtAuthFilter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true) // estas 2 @ deben ir juntas en Spring Boot 3.0
+@RequiredArgsConstructor // lombok to generate constructor with required fields DI
+public class WebSecurityConfig {
+
+    private final JwtAuthFilter jwtAuthFilter;
+    private final AuthenticationProvider authenticationProvider; // <-- JwtAppConfig
+    private final JwtAuthEntryPoint jwtAuthEntryPoint;
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .exceptionHandling(exceptionHandling ->
+                        exceptionHandling
+                                .authenticationEntryPoint(jwtAuthEntryPoint))
+                .authorizeHttpRequests(authRequest ->
+                        authRequest
+                                .requestMatchers("/api/v1/auth/**", "/api/v1/home/**").permitAll()
+                                .anyRequest().authenticated()
+                )
+                .sessionManagement(sessionManager ->
+                        sessionManager
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationProvider(authenticationProvider)
+                .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class)
+                .build();
+
+    }
+
+}
