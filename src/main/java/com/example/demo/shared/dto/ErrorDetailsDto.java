ab542834aa852835ae4e4dc4783ea58ea5e81d71
Adrian <alextravc.342@gmail.com>
2024-06-15 20:50:44 -0500

    ADD: Exceptions

---
+++ src/main/java/com/example/demo/shared/dto/ErrorDetailsDto.java
@@ -0,0 +1,21 @@
+package com.example.demo.shared.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class ErrorDetailsDto {
+
+    private Date timeStamp;
+    private String message;
+    private String details;
+
+}
